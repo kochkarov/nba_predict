2849649700817aa386e9b6db92693263e8199f57
kochkarov <kochkarev@gmail.com>
2021-05-31 03:09:20 +0300

    Added static files

--- nba_predict/settings.py
+++ nba_predict/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "bnfg1&$^+bzm$^m)^te&6%&7h)luh(q(euhhp5m148@&%#sb9%"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.9']
+ALLOWED_HOSTS = ['192.168.0.9', '192.168.0.11', '127.0.0.1', 'nba.ping.kiev.ua']
 
 # Application definition
 
@@ -128,5 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = "dashboard"
