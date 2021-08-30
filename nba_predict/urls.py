6ebde15b2fed734ff627030753427b1c05dd9627
kochkarov <kochkarev@gmail.com>
2021-08-30 20:51:20 +0300

    Remove temporary files

--- nba_predict/urls.py
+++ nba_predict/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games', include('game.urls')),
     path('user/', include('member.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('', include('championship.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
